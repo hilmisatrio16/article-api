71e89dcb965562601fb6a6fee1a431e0790f4888
muhammadhilmisatrio <hilmisatrio16@gmail.com>
2023-12-14 02:56:16 +0700

    edites

--- routes/articlerouter.js
+++ routes/articlerouter.js
@@ -4,10 +4,10 @@ const router = express.Router()
 const articleController = require('../controllers/articlecontroller')
 
 router.get("/", articleController.getAll)
-router.get("/", articleController.getById)
+router.get("/:id", articleController.getById)
 router.post("/", articleController.insertArticle)
-router.put("/", articleController.updateArticleById)
-router.delete("/", articleController.deleteArticleById)
+router.put("/:id", articleController.updateArticleById)
+router.delete("/:id", articleController.deleteArticleById)
 
 module.exports = router
 
